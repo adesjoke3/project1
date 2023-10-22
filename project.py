90f3dbd76a9a660043c193f70345c7a9a59e687c
adesjoke3 <147931807+adesjoke3@users.noreply.github.com>
2023-10-22 15:44:37 +0000

    first commit

---
+++ project.py
@@ -0,0 +1,38 @@
+name = "Adejoke"
+print(name)
+
+age = 9
+print(age)
+
+favorite_movie = "julie_and_the_phantoms"
+print(favorite_movie)
+
+favorite_color = "None"
+print(favorite_color)
+
+favorite_subject = "math"
+print(favorite_subject)
+
+i_like_icecream = True
+print(i_like_icecream)
+
+i_like_orange = True
+print(i_like_orange)
+
+i_like_bananas = True
+print(i_like_bananas)
+
+birth_month = "july"
+print(birth_month)
+
+height = 4.4
+print(birth_month)
+
+shoe_size = 6
+print(shoe_size)
+
+siblings = 3
+print(siblings)
+
+bestfriend_name = "Dara"
+print(bestfriend_name)
