be7293d947fb2469910ee4044bd42d06b45a3b60
adesjoke3 <147931807+adesjoke3@users.noreply.github.com>
2023-11-18 15:27:55 +0000

    Added new files

--- project.py
+++ project.py
@@ -36,3 +36,34 @@ print(siblings)
 
 bestfriend_name = "Dara"
 print(bestfriend_name)
+
+x= 10
+y=15
+
+if x ==y:
+    print("x is equal to y")
+else:
+    print("x is not equal to y")
+
+    num1 = 200
+    num2 = 500
+    if num1  num2:
+
+print("num1 is greater then num2")
+else:
+print("num1 is not greater then num2")
+
+age = 17
+
+if age != 18:
+    print("you are not 18 years old")
+else:
+    print("you are 18 years old")
+
+temperature = 25
+if temperature  30
+    print("it is a cold day")
+else:
+    print("it is not a cold day")
+
+        
